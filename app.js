d35d845ed0a91d528ec044698a1e175b7ba630e5
Arun Maurya <arunmaurya621@gmail.com>
2022-05-24 04:30:57 +0530

    added files

--- app.js
+++ app.js
@@ -2,30 +2,40 @@ const  shortid = require('shortid');
 const express=require('express')
 const qrcode=require('qrcode')
 const path=require('path')
+const request=require('request')
 
 const pool=require('./DB/pools')
 
 const app=express();
 
-// app.use('/',express.static('/views/generate'));
+app.use('/',express.static('/views/generate'));
 // app.use('/qrcode',express.static('/views/qrcode'))
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine','ejs')
-// app.use(express.static(__dirname + '/views'));
+
 app.use(express.static('/views'))
+
+request.get('https://www.google.com',(err,res,data)=>{
+    if(!err) console.log(res.statusCode);
+})
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/generate/index.html'))
 })
 
 app.post('/qr/generate',(req,res)=>{
+
     console.log(JSON.parse(JSON.stringify(req.body)).full);
     pool.getConnection((err,connection)=>{
         if(err) throw err;
         let shortUrl=shortid.generate()
+
         console.log("URL generated "+shortUrl);
+
         let sql=`insert into shortUrl (full,short) values(?,?)`
         connection.query(sql,[JSON.parse(JSON.stringify(req.body)).full,shortUrl],(error)=>{
             connection.release();
@@ -45,34 +55,23 @@ app.post('/qr/generate',(req,res)=>{
         
     })
  })
-app.get('/:shortURL',(req,res)=>{
-    pool.getConnection((err,connection)=>{
-        if(err) throw err;
-        let sql1=`select id,full from shortUrl where short=?`
+ 
 
-        connection.query(sql1,req.params.shortURL,(error,results)=>{
-            connection.release();
-            if(results) res.redirect(results[0].full)
-            else res.redirect('/')
-            if(error) throw error;
-            
-            
-        })
-    })
-})
-
-app.post('/qr/edit/:shortUrl',(req,res)=>{
+app.get('/qr/edit/:shortUrl',(req,res)=>{
     pool.getConnection((err,connection)=>{
         if(err) throw err;
         
         let sql2=`update shortUrl set full=? where short= ?`
-        connection.query(sql2,[req.body.full,req.params.shortUrl],(err,results)=>{
+        connection.query(sql2,['https://www.amazon.in',req.params.shortUrl],(err,results)=>{
             connection.release();
             if(err) throw err;
-            res.redirect(`/${shortUrl}`)
+            res.redirect(`/${req.params.shortUrl}`)
         })
     })
 })
+
+
+
 
 
 app.listen(6900,()=>{

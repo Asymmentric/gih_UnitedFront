b4df88915e7d598af55531b15d1762db6cd7093b
Arun Maurya <arunmaurya621@gmail.com>
2022-05-23 17:03:54 +0530

    ohkay

---
+++ app.js
@@ -0,0 +1,19 @@
+var shortid = require('shortid');
+const express=require('express')
+const qrcode=require('qrcode')
+
+const app=express();
+
+console.log(shortid.generate())
+
+let qr=qrcode.toDataURL('https://www.instagram.com/aniruddha_bagal',{type:'terminal'},(err,url)=>{
+    console.log(url)
+})
+app.get('/someUrl',(req,res)=>{
+    // console.log(req)
+    // res.send(qr)
+})
+
+app.listen(6900,()=>{
+    console.log('Running')
+})

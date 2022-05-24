fcc836657db41e68a2ee243b9b6759630aabc212
Arun Maurya <arunmaurya621@gmail.com>
2022-05-24 22:55:51 +0530

    OKAY

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const pool=require('./DB/pools')
 const app=express();
 
 app.use('/',express.static('client/generate'));
+app.use('/qrcode',express.static('client/qrcode'))
+app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 
-let qr=qrcode.toDataURL('https://www.instagram.com/aniruddha_bagal',(err,url)=>{
-    return url;
-})
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/client/generate/index.html'))
 })
@@ -30,9 +30,31 @@ app.get('/generate',(req,res)=>{
                 console.log(error);
                 res.send({"message":"Unable to generate"})
             }
+            res.redirect(`/qrcode/${shortUrl}`)
+        })  
+    })
+})
+app.get('/:shortURL',(req,res)=>{
+    pool.getConnection((err,connection)=>{
+        if(err) throw err;
+        let sql1=`select id,full from shortUrl where short= ?`
+        connection.query(sql1,req.params,(error,results)=>{
+            connection.release();
+            if(error) throw error;
+            console.log(results)
+res.send({"data":"lol"})
         })
     })
 })
+app.get('/qrcode/:shortUrl',(req,res)=>{
+
+    res.sendFile(path.join(__dirname,'/client/qrcode/qr.html'))
+})
+
+app.get('/edit',(req,res)=>{
+    console.log(req.body)
+})
+
 
 app.listen(6900,()=>{
     console.log('Running')
